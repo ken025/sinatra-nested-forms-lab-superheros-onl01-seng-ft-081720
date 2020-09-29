72aedafed83d2093a7ac78f2e245e7f378d0ce96
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-29 04:40:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,22 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do
+   
+    erb :super_hero
+  end
 
+    post '/teams' do
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+       
+      params[:team][:heroes].each do |hero| 
+            
+          Hero.new(hero)
+     end
+        
+        @heros = Hero.all
+        
+      erb :team
+  end
 end
